91995b82c1c2648dbe9d877246d303c6c8d98918
harpreetss8090 <36657936+harpreetss8090@users.noreply.github.com>
2020-04-18 13:17:58 -0300

    Update MainClass.java

--- src/MainClass.java
+++ src/MainClass.java
@@ -7,7 +7,7 @@ public class MainClass {
 	public static void main(String[] args) throws IOException {
 		Mathdoku md = new Mathdoku();
 		
-		// TODO Assign the text file path to inputfilepath eg. C:\\Users\\Harpreet\\Desktop\\mathdoku.txt
+		// TODO Assign the text file path to inputfilepath eg. C:\\Users\\Harpreet\\Desktop\\test1.txt
 		
 		String inputfilepath = "";
 		
